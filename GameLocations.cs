aaa9c2a5ae1e31f5bab8df98f8648f181a5a0be8
jakeS03 <61432337+jakeS03@users.noreply.github.com>
2020-02-25 11:28:08 -0800

    Add files via upload

---
+++ GameLocations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WumpusTest
+{
+    class GameLocations
+    {
+        int[] roomsWithBats = new int[2];
+        int[] roomsWithBottomlessPits = new int[2];
+        int roomWithWumpus = 0;
+
+        public GameLocations()
+        {
+            // sets new rooms blah blah
+        }
+
+        public bool movePlayer(int newRoom)
+        {
+            // moves player to new room, returns bool if succesful
+            return true;
+        }
+
+        public bool moveWumpus(int newRoom)
+        {
+            // moves Wumpus to new room
+            return true;
+        }
+
+        public bool testForHazards()
+        {
+            // returns true if hazard
+            return true;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WumpusTest
+{
+    class Player
+    {
+        private int arrows = 5;
+        private int coins = 150;
+        private int turns = 0;
+
+        public Player()
+        {
+            arrows = 5;
+            coins = 150;
+            turns = 0;
+
+        }
+        public int getNumberOfArrows()
+        {
+            return arrows;
+            // returns number of arrows in inventory
+        }
+        public int getCoins()
+        {
+            return coins;
+            // returns number of coins
+        }
+        public void addArrow()
+        {
+            arrows += 1;
+            // adds one arrow to inventory
+        }
+        public bool useArrow()
+        {
+            if (arrows >= 1)
+            {
+                arrows -= 1;
+                return true;
+            } else
+            {
+                return false;
+            }
+            // returns true if successfully used an arrow and false if failed
+        }
+        public void addCoins(int coins)
+        {
+            this.coins += coins;
+            // adds parameter of coins to coin total
+        }
+        public void spendCoins(int coins)
+        {
+            this.coins -= coins;
+            // subtracts parameter from coin total
+        }
+        public void addTurn()
+        {
+            turns += 1;
+            // adds one turn to total turns
+        }
+        public int computeScore()
+        {
+            return -1;
+            // computes total score
+        }
+        public int getTurns()
+        {
+            return turns;
+            // returns number of turns
+        }
+    }
+}
